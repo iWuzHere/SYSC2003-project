7c698b41ca52c2fd665a0ec59c2a89cd48b96a22
Brendan MacDonell <brendan@macdonell.net>
2011-03-08 16:52:50 -0500

    Add missing macros to stepper.c

--- a4/stepper.c
+++ a4/stepper.c
@@ -3,6 +3,12 @@
 typedef unsigned char byte;
 typedef unsigned char boolean;
 
+/* Bit-set and clear operations. Could alternatively *
+ * use the ASM directive if these are too slow.      */
+#define SETMSK(var, msk) ((var) |= (msk))
+#define CLRMSK(var, msk) ((var) &= ~(msk))
+#define TOGGLEMASK(var, msk) ((var) ^= (msk))
+
 #define MOTOR_OUTPUT_MASK 0x60
 #define MOTOR_ENABLE_MASK 0x20
 #define SEVEN_SEG_EN_MASK 0x04
@@ -12,7 +18,7 @@ static const char stepper_motor_values[] = {
   0x40, // Up and right.
   0x00, // Down and right.
   0x20  // Down and left.
-}
+};
 
 /* step_motor(int steps, boolean clockwise):                    *
  * Move the stepper motor 'steps' steps in the given direction. */
